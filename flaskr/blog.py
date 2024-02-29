4df58f15e42c71ac9e8d0c434569b74bd17753f8
sullivanmsean <sullivan.sean2@northeastern.edu>
2024-02-29 16:20:27 -0500

    refactoring to bootstrap for UI

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -6,6 +6,9 @@ from werkzeug.exceptions import abort
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
+from itertools import groupby
+from operator import itemgetter
+
 bp = Blueprint('blog', __name__)
 
 @bp.route('/')
@@ -14,9 +17,21 @@ def index():
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username, listing'
         ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
+        ' ORDER BY listing, created DESC'
     ).fetchall()
-    return render_template('blog/index.html', posts=posts)
+    
+    # Ensure posts is a list of dicts for easier manipulation
+    posts = [dict(post) for post in posts]
+    
+    # Apply the parsing function to each 'listing' URL
+    for post in posts:
+        if post['listing']:
+            post['parsed_address'] = parse_address_from_listing(post['listing'])
+
+    # Group posts by listing
+    grouped_posts = {k: list(v) for k, v in groupby (posts, key=itemgetter('listing'))}
+    
+    return render_template('blog/index.html', grouped_posts=grouped_posts)
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
@@ -60,6 +75,8 @@ def get_post(id, check_author=True):
 
     return post
 
+
+
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
 def update(id):
@@ -96,3 +113,15 @@ def delete(id):
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
+
+
+def parse_address_from_listing(listing_url):
+    # Unicode for the house emoji
+    house_emoji = '\U0001F3E0'
+    # Split the URL by slashes and take the part that contains the address
+    parts = listing_url.split('/')
+    address_part = parts[4]  # This is based on the URL structure you've given
+     # Replace dashes with spaces
+    address = address_part.replace('-', ' ')
+    # Prepend the house emoji to the formatted address and make address a hyperlink
+    return f"{house_emoji} <a href='{listing_url}' target='_blank'>{address}</a>"
